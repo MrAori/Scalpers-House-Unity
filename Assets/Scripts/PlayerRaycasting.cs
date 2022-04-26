d48c0e16c192b4556a5cc3a3c353b8fdf2657e90
MrAori <42550826+MrAori@users.noreply.github.com>
2022-04-26 12:06:48 +0100

    Added PlayerRaycasting
    This script will basically handle the main interaction stage of the project

---
+++ Assets/Scripts/PlayerRaycasting.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerRaycasting : MonoBehaviour
+{
+    public float distanceToSee;
+    RaycastHit whatIHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.magenta);
+
+        if(Physics.Raycast(this.transform.position, this.transform.forward, out whatIHit, distanceToSee))
+        {
+            Debug.Log("Currently touching " +whatIHit.collider.gameObject.name);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Pressed primary button.");
+
+                if (whatIHit.collider.name.Equals("OtherCylinder"))
+                {
+                    GameObject.Find("Text").GetComponent<Text>().text = "Price: $200";
+                }
+                else if (whatIHit.collider.name.Equals("Cylinder"))
+                {
+                    GameObject.Find("Text").GetComponent<Text>().text = "Price: $100";
+                }
+
+            }
+        }
+        else
+        {
+            GameObject.Find("Text").GetComponent<Text>().text = "";
+        }
+    }
+}
